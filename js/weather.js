fe243aad52d78f0cadb0bb92004dcd8226751ecf
jeongyugyeong22 <jyg1022@naver.com>
2020-08-16 12:28:35 +0900

    update api

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "4eb4bbde64699e10ea0bd8450175f218";
 const COORDS = 'coords';
 
 function getWeather(lat, lng){
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
     ).then(function(response){
         return response.json();
     }).then(function(json){
